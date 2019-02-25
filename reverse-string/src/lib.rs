df4875617bafeff5fce1b03b696054aabbc2d7b1
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-02-25 14:26:20 -0700

    Comments

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,4 +1,8 @@
 pub fn reverse(input: &str) -> String {
+    // Initialize a new string because we don't want to mutate the original one
+    // In order to iterate over the characters in a string use the .chars() method
+    // to get the iterator. Rust iterators have a .rev() method to go in the
+    // reverse order
     let mut result = String::with_capacity(input.len());
     for character in input.chars().rev() {
         result.push(character);
