f295ffe5a928d99d5ee22ced160c627b46bdae8d
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-02-25 14:07:30 -0700

    Formatting

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -13,13 +13,11 @@ pub fn find(sum: u32) -> HashSet<[u32; 3]> {
     // mathematically
 
     let mut set: HashSet<[u32; 3]> = HashSet::with_capacity(3);
-    for a in 1..=sum/3 {
-        for b in a + 1 ..=sum/2 {
+    for a in 1..=sum / 3 {
+        for b in a + 1..=sum / 2 {
             let c = sum - a - b;
             if a.pow(2) + b.pow(2) == c.pow(2) {
-                let result: [u32;3] = [
-                    a, b, c
-                ];
+                let result: [u32; 3] = [a, b, c];
                 set.insert(result);
             }
         }
