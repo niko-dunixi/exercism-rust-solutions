ac30d20d3cc8959a8c47117b7f67663593c1dd11
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-02-25 16:54:23 -0700

    Wrote a method to determine if a number is prime and then used it as I counted prime numbers

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,3 +1,26 @@
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    let mut prime_count = 1;
+    let mut x = 2;
+    while prime_count <= n {
+        x += 1;
+        if is_prime(x) {
+            prime_count += 1;
+        }
+    }
+    return x;
+}
+
+pub fn is_prime(n: u32) -> bool {
+    if n < 2 {
+        return false;
+    } else if n == 2 {
+        return true;
+    }
+    let sqrt = (n as f64).sqrt() as u32;
+    for x in 2..=sqrt {
+        if n % x == 0 {
+            return false;
+        }
+    }
+    return true;
 }
