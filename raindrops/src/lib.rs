45fa266c65d22c71403f2e3ca435738332bb13c4
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-02-26 12:00:49 -0700

    Adding implementation to pass first test

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,7 +1,21 @@
 use std::collections::HashSet;
 
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut result = String::new();
+    let factors = determine_factors(n);
+    if factors.contains(&3) {
+        result += "Pling";
+    }
+    if factors.contains(&5) {
+        result += "Plang";
+    }
+    if factors.contains(&7) {
+        result += "Plong";
+    }
+    if result.is_empty() {
+        result = n.to_string();
+    }
+    return result;
 }
 
 pub fn determine_factors(n: u32) -> HashSet<u32> {
