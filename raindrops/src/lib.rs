0d7070a6e08dd3c7e29b153888a85f53f1e720ae
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-02-26 11:56:10 -0700

    Adding implementation to find factors

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,3 +1,15 @@
+use std::collections::HashSet;
+
 pub fn raindrops(n: u32) -> String {
     unimplemented!("what sound does Raindrop #{} make?", n)
+}
+
+pub fn determine_factors(n: u32) -> HashSet<u32> {
+    let mut set = HashSet::new();
+    for value in 1..=n {
+        if n % value == 0 {
+            set.insert(value);
+        }
+    }
+    return set;
 }
