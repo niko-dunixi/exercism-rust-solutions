e1ecbdf33cc859d038a5eaa3799f6bbf5dd80719
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-02-26 13:57:36 -0700

    Implementing methods utilizing iterators

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,14 +1,11 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    unimplemented!("square of sum of 1...{}", n)
+    return (1..=n).sum::<u32>().pow(2);
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{}", n)
+    return (1..=n).map(|x| x.pow(2)).sum::<u32>();
 }
 
 pub fn difference(n: u32) -> u32 {
-    unimplemented!(
-        "difference between square of sum of 1...{n} and sum of squares of 1...{n}",
-        n = n,
-    )
+    return square_of_sum(n) - sum_of_squares(n);
 }
