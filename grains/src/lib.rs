4905bc5220348dbe5034a9a39aa7c872ef9990a9
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-02-26 10:13:22 -0700

    Using bit-shifting to solve problem

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,5 +1,8 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    if s < 1 || s > 64 {
+        panic!("Square must be between 1 and 64")
+    }
+    return 1u64.rotate_left(s - 1);
 }
 
 pub fn total() -> u64 {
