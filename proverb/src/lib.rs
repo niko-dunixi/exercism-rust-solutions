b29efc3db3e36959c8bb2de1b292e06076c73b87
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-02-27 09:12:24 -0700

    Implementing exercise

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,15 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    if list.is_empty() {
+        return String::new();
+    }
+    let mut result = String::new();
+    for i in 1..list.len() {
+        let intermediate_string = format!("For want of a {} the {} was lost.", list[i - 1], list[i]);
+        result.push_str(&intermediate_string);
+        result.push('\n');
+    }
+    let first_item = list[0];
+    let last_statement = format!("And all for the want of a {}.", first_item);
+    result.push_str(&last_statement);
+    return result;
 }
